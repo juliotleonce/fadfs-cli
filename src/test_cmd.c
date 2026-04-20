65f9ce4efe0aca347c72ae80be9b6e1a0bbecb5b
JuliotLeonce <juliotleonce123@gmail.com>
2026-04-20 23:29:20 +0300

    feat(main): changed test command to use

--- src/test_cmd.c
+++ src/test_cmd.c
@@ -1,9 +1,9 @@
+#include <stdio.h>
 #include <xstdlib/xstring.h>
 
 #include "../include/cmd_def.h"
 
 void test_cmd_run(ReplContext *ctx,const XArray *cmd_args) {
     const XString *message = xarray_at(cmd_args, 0);
-    fprintf(ctx->fadisk_ptr, "%s\n", message->c_str);
-    fflush(ctx->fadisk_ptr);
+    printf("%s\n", message->c_str);
 }
