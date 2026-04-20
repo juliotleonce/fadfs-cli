1176d730cbd9b8ceec06036f4129c39254091cce
JuliotLeonce <juliotleonce123@gmail.com>
2026-04-20 23:29:02 +0300

    feat(main): implemented new repl structure

--- main.c
+++ main.c
@@ -3,8 +3,9 @@
 
 int main(int argc, char** argv) {
     ReplContext *ctx = repl_init(argc, argv);
+    repl_setup_cmd(ctx);
+    repl_before_run(ctx);
     repl_run(ctx);
-
     xmem_reset();
     return 0;
 }
